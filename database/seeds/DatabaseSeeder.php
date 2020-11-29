84a2e2dda242f48727c32330e399cff9cc741f58
Noel Daz Jr <nmdazgames@gmail.com>
2020-11-30 07:55:24 +0800

    Added faker to dep

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -36,45 +36,5 @@ class DatabaseSeeder extends Seeder
             'email' => 'email02@gmail.com',
             'password' => 'pass1234'
         ]);
-
-        // for ($a = 0; $a < 7; $a++) {
-        //     $course1 = factory(Course::class)->create([
-        //         'author' => $user->id
-        //     ]);
-
-        //     $section1 = factory(Section::class)->create([
-        //         'course_id' => $course1->id
-        //     ]);
-        //     $lesson1 = factory(Lesson::class)->create([
-        //         'section_id' => $section1->id
-        //     ]);
-
-        //     $comment = factory(Comment::class)->create([
-        //         'user_id' => $user->id,
-        //         'lesson_id' => $lesson1->id
-        //     ]);
-
-        //     $lesson11 = factory(Lesson::class)->create([
-        //         'section_id' => $section1->id
-        //     ]);
-        //     $section1->lessons()->save($lesson1);
-        //     $section1->lessons()->save($lesson11);
-
-        //     $course1->sections()->save($section1);
-
-        //     $section2 = factory(Section::class)->create([
-        //         'course_id' => $course1->id
-        //     ]);
-        //     $lesson2 = factory(Lesson::class)->create([
-        //         'section_id' => $section2->id
-        //     ]);
-        //     $lesson22 = factory(Lesson::class)->create([
-        //         'section_id' => $section2->id
-        //     ]);
-        //     $section2->lessons()->save($lesson2);
-        //     $section2->lessons()->save($lesson2);
-
-        //     $course1->sections()->save($section2);
-        // }
     }
 }
