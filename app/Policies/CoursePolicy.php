1d672530d4fb9dbd4f1011fba1dc79d243a1ddf5
Noel Daz Jr <nmdazgames@gmail.com>
2020-12-04 00:06:09 +0800

    Added Google Drive Policy

---
+++ app/Policies/CoursePolicy.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Policies;
+
+use App\User;
+use App\Course;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class CoursePolicy
+{
+    use HandlesAuthorization;
+
+    /**
+     * Create a new policy instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function update(User $user, Course $course) {
+        return $user->id === $course->author_id;
+    }
+}
